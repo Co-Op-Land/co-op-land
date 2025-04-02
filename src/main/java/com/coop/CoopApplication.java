d1db36d79062308951160c0b17b81593319045be
lcy <plmokn48@daum.net>
2025-04-02 17:25:27 +0900

    add: jwt properties 설정파일 추가

--- src/main/java/com/coop/CoopApplication.java
+++ src/main/java/com/coop/CoopApplication.java
@@ -1,11 +1,14 @@
 package com.coop;
 
+import com.coop.global.security.JwtSecurityProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties({JwtSecurityProperties.class})
 public class CoopApplication {
 
 	public static void main(String[] args) {
