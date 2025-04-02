62b07ba0e66df19c6981701c89862f61c7e8a02a
lcy <plmokn48@daum.net>
2025-04-02 17:28:09 +0900

    delete: 사용하지 않는 코드 삭제

--- src/main/java/com/coop/presentation/post/controller/PostController.java
+++ src/main/java/com/coop/presentation/post/controller/PostController.java
@@ -1,12 +1,7 @@
 package com.coop.presentation.post.controller;
 
 import com.coop.domain.post.service.PostService;
-import com.coop.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +11,4 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
-
-    @PostMapping
-    public ResponseEntity<ApiResponse<Long>> createPost(@AuthenticationPrincipal UserDetails userDetails) {
-        Long id = postService.generatePost();
-        return ApiResponse.created(id);
-    }
 }
