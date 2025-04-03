b566d5612fc663afb3f7b01c036ac3f2f03e4ddc
lcy <plmokn48@daum.net>
2025-04-03 16:02:50 +0900

    refactor: historyResponse에서 memberResponse를 사용하도록 수정

--- src/main/java/com/coop/presentation/member/dto/response/MemberResponse.java
+++ src/main/java/com/coop/presentation/member/dto/response/MemberResponse.java
@@ -1,14 +1,18 @@
 package com.coop.presentation.member.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record MemberResponse(
+        Long id,
         String email,
         String name
 ) {
-    public static MemberResponse from(String email, String name) {
+    public static MemberResponse from(Long id, String email, String name) {
         return MemberResponse.builder()
+                .id(id)
                 .email(email)
                 .name(name)
                 .build();
--- src/main/java/com/coop/presentation/playHistory/dto/response/HistoryResponse.java
+++ src/main/java/com/coop/presentation/playHistory/dto/response/HistoryResponse.java
@@ -2,17 +2,27 @@ package com.coop.presentation.playHistory.dto.response;
 
 import com.coop.domain.playHistory.entity.History;
 import com.coop.domain.player.entity.Player;
+import com.coop.presentation.member.dto.response.MemberResponse;
 import lombok.Builder;
 
 import java.util.List;
+
 @Builder
-public record HistoryResponse(Long historyId, List<String> nickname) {//여기에 memberId 대신에 dto를 넣어서 memberId, memberNickname정도를 넣고 싶어요.
+public record HistoryResponse(
+        Long historyId,
+        List<MemberResponse> members
+) {
+
     public static HistoryResponse from(History history, List<Player> players) {
         return HistoryResponse.builder()
                 .historyId(history.getId())
-                .nickname(players.stream()
-                        .map(player -> player.getMember().getNickname())
+                .members(players.stream()
+                        .map(player -> {
+                            var member = player.getMember();
+                            return MemberResponse.from(member.getId(), null, member.getNickname());
+                        })
                         .toList())
                 .build();
     }
 }
+
