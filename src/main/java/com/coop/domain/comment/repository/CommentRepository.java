2baff1b191deda28c609561e9c7dd85ef9d8c06b
goo3oo <goo3oo0918@gmail.com>
2025-04-03 20:48:57 +0900

    feat: Comment 도메인 초기 구성

--- src/main/java/com/coop/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/coop/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package com.coop.domain.comment.repository;
 
 import com.coop.domain.comment.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findAllByMember_Id(Long memberId, Pageable pageable);
+    Page<Comment> findAllByPost_Id(Long postId, Pageable pageable);
+    Page<Comment> findAllByPost_IdAndParentIsNull(Long postId, Pageable pageable);
 }
