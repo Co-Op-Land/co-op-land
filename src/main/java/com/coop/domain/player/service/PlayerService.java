e3f44f58f4df7f3adad452f0959955cfd2f234e6
chk223 <kch23_71@naver.com>
2025-04-03 12:05:01 +0900

    feat: Player 조회, 생성 구현

--- src/main/java/com/coop/domain/player/service/PlayerService.java
+++ src/main/java/com/coop/domain/player/service/PlayerService.java
@@ -6,6 +6,7 @@ import com.coop.domain.player.entity.Player;
 import com.coop.domain.player.repository.PlayerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class PlayerService {
 
     private final PlayerRepository playerRepository;
 
+    @Transactional
     public void generatePlayer(History history, List<Member> members) {
         for(Member member : members){
             Player player = Player.builder()
