a1cea8a2a9ad692494f55e8c5b7e4e3b637f0507
lcy <plmokn48@daum.net>
2025-04-03 12:09:03 +0900

    feat: member 조회, 비번 변경 구현

--- src/main/java/com/coop/presentation/member/controller/MemberController.java
+++ src/main/java/com/coop/presentation/member/controller/MemberController.java
@@ -1,11 +1,38 @@
 package com.coop.presentation.member.controller;
 
+import com.coop.domain.member.service.MemberService;
+import com.coop.global.common.ApiResponse;
+import com.coop.presentation.member.dto.request.UpdatePasswordRequest;
+import com.coop.presentation.member.dto.response.MemberResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<MemberResponse>> readUser(
+            @AuthenticationPrincipal User userDetails
+    ) {
+        Long userId = Long.valueOf(userDetails.getUsername());
+        MemberResponse responseDto = memberService.readUser(userId);
+        return ApiResponse.success(responseDto);
+    }
+
+    @PatchMapping("/password")
+    public ResponseEntity<ApiResponse<String>> updatePassword(
+            @AuthenticationPrincipal User userDetails,
+            @RequestBody UpdatePasswordRequest requestDto
+    ) {
+        Long userId = Long.valueOf(userDetails.getUsername());
+        memberService.updatePassword(userId, requestDto);
+        return ApiResponse.success("비밀번호 변경 성공");
+    }
 }
