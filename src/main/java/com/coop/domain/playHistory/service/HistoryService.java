c7e1e5ca15a4494731ad300ab2415719315953f9
chk223 <kch23_71@naver.com>
2025-04-03 14:05:39 +0900

    feat: player 의존하도록 구현

--- src/main/java/com/coop/domain/playHistory/service/HistoryService.java
+++ src/main/java/com/coop/domain/playHistory/service/HistoryService.java
@@ -1,25 +1,35 @@
 package com.coop.domain.playHistory.service;
 
+import com.coop.domain.member.entity.Member;
 import com.coop.domain.playHistory.entity.History;
 import com.coop.domain.playHistory.repository.HistoryRepository;
+import com.coop.domain.player.service.PlayerService;
 import com.coop.domain.room.entity.Room;
-import com.coop.domain.room.repository.RoomRepository;
-import com.coop.global.exception.error.NotFoundException;
+import com.coop.presentation.playHistory.dto.response.HistoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class HistoryService {
 
     private final HistoryRepository historyRepository;
-    private final RoomRepository roomRepository;
+    private final PlayerService playerService;
 
-    public History generateHistory(Long room_id) {
-        Room room = roomRepository.findById(room_id).orElseThrow(NotFoundException::new);
+    public History generateHistory(Room room, List<Member> members) {
         History history = History.builder()
                 .room(room)
                 .build();
+        generatePlayer(history,members);
         return historyRepository.save(history);
+    }
+
+    private void generatePlayer(History history, List<Member> members) {
+        playerService.generatePlayer(history,members);
+    }
+
+    public void findMemberHistories(Long memberId) {
     }
 }
