dea233c5268b1406752e157a58d284a39516db22
goo3oo <goo3oo0918@gmail.com>
2025-04-03 20:49:55 +0900

    feat: Post 도메인 초기 구성

--- src/main/java/com/coop/presentation/post/dto/request/PostRequest.java
+++ src/main/java/com/coop/presentation/post/dto/request/PostRequest.java
@@ -1,4 +1,18 @@
 package com.coop.presentation.post.dto.request;
 
-public class PostRequest {
+import com.coop.domain.member.entity.Member;
+import com.coop.domain.post.entity.Post;
+import com.coop.domain.post.enums.PostCategory;
+import lombok.Builder;
+
+@Builder
+public record PostRequest(String title, String content, String category) {
+    public Post of(Member member) {
+        return Post.builder()
+                .member(member)
+                .title(title)
+                .content(content)
+                .category(PostCategory.of(category))
+                .build();
+    }
 }
