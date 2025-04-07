f58196e16b787dce90e4e5340d8df5b08004ff27
lcy <plmokn48@daum.net>
2025-04-07 15:34:13 +0900

    feat: 댓글 이벤트 consumer구현

---
+++ src/main/java/com/coop/global/notification/NotificationConsumer.java
@@ -0,0 +1,32 @@
+package com.coop.global.notification;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class NotificationConsumer {
+
+    private final RedisTemplate<String, String> redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "notification.comment", groupId = "notification-group")
+    public void consume(NotificationEvent event) {
+        try {
+            String redisChannel = "noti:member:" + event.getToMemberId();
+
+            String payload = objectMapper.writeValueAsString(event);
+            redisTemplate.convertAndSend(redisChannel, payload);
+
+            log.info("Kafka → Redis Pub/Sub 전송 완료: {}, 채널: {}", event, redisChannel);
+        } catch (Exception e) {
+            log.error("Redis Pub/Sub 전송 실패", e);
+            //TODO: retry topic 전송도 고려 가능
+        }
+    }
+}
