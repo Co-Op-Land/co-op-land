c218fd65ee7eb33cbdc6f62f2f10ce287ed3d178
lcy <plmokn48@daum.net>
2025-04-07 16:23:38 +0900

    feat: 알림 리스너(redis -> websocketsession) 구현

---
+++ src/main/java/com/coop/global/notification/NotificationListener.java
@@ -0,0 +1,27 @@
+package com.coop.global.notification;
+
+import com.coop.global.websocket.WebSocketSessionManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationListener implements MessageListener {
+
+    private final WebSocketSessionManager sessionManager;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String channel = new String(message.getChannel());
+        String body = new String(message.getBody());
+        Long memberId = extractMemberIdFromChannel(channel);
+
+        sessionManager.sendNotificationToMember(memberId, body);
+    }
+
+    private Long extractMemberIdFromChannel(String channel) {
+        return Long.parseLong(channel.replace("noti:member:", ""));
+    }
+}
