28e89036ab858c80549347aa964a7c86c44a9fcb
lcy <plmokn48@daum.net>
2025-04-13 14:19:16 +0900

    add: notification 도메인 패키지 생성

---
+++ src/main/java/com/coop/domain/notification/Notification.java
@@ -0,0 +1,44 @@
+package com.coop.domain.notification;
+
+import com.coop.domain.member.entity.Member;
+import com.coop.global.common.BaseEntity;
+import com.coop.global.notification.values.NotificationTarget;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notification")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long related_id;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationTarget target;
+
+    @ManyToOne
+    @JoinColumn(name = "from_member_id")
+    private Member fromMember;
+
+    @ManyToOne
+    @JoinColumn(name = "to_member_id")
+    private Member toMember;
+
+    private boolean isRead = false;
+
+    @Builder
+    public Notification(Long related_id, NotificationTarget target, Member fromMember, Member toMember) {
+        this.related_id = related_id;
+        this.target = target;
+        this.fromMember = fromMember;
+        this.toMember = toMember;
+    }
+}
---
+++ src/main/java/com/coop/domain/notification/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.coop.domain.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
