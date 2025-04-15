95d19ba65e584d885baeace147e9a58689e27b50
lcy <plmokn48@daum.net>
2025-04-15 19:29:23 +0900

    fix: 현재 접속중인 유저에게만 websocket알림 보내도록 수정

--- src/main/java/com/coop/global/notification/core/NotificationConsumer.java
+++ src/main/java/com/coop/global/notification/core/NotificationConsumer.java
@@ -2,12 +2,15 @@ package com.coop.global.notification.core;
 
 import com.coop.global.notification.values.NotificationEvent;
 import com.coop.global.notification.values.NotificationMessage;
+import com.coop.global.websocket.WebSocketSessionManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,22 +19,28 @@ public class NotificationConsumer {
 
     private final RedisTemplate<String, String> redisTemplate;
     private final ObjectMapper objectMapper;
+    private final WebSocketSessionManager sessionManager;
 
-    /**
-     * Kafka 에서 Redis Pub/Sub 으로 event 를 발송하는 메서드
-     */
     @KafkaListener(topicPattern = "notification\\..*", groupId = "notification-group")
     public void consume(NotificationEvent event) {
         try {
             NotificationMessage message = NotificationMessage.from(event);
             String payload = objectMapper.writeValueAsString(message);
-            for (Long toMemberId : event.toMemberIds().getValues()) {
+
+            //현재 접속 중인 유저만 필터링
+            List<Long> connectedUserIds = sessionManager.getConnectedUserIdList();
+            List<Long> toSendIds = event.toMemberIds().getValues().stream()
+                    .filter(connectedUserIds::contains)
+                    .toList();
+
+            for (Long toMemberId : toSendIds) {
                 String redisChannel = "noti:member:" + toMemberId;
                 redisTemplate.convertAndSend(redisChannel, payload);
             }
+
         } catch (Exception e) {
             log.error("Redis Pub/Sub 전송 실패", e);
-            //TODO: retry topic 전송도 고려 가능
         }
     }
 }
+
